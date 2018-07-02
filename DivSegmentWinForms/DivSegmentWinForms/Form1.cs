5caaf982579a897bb14e23240dc0f671fd44e1ef
AnastasiiaFil <40761069+AnastasiiaFil@users.noreply.github.com>
2018-07-02 22:19:11 +0300

    Added comments to code

--- DivSegmentWinForms/DivSegmentWinForms/Form1.cs
+++ DivSegmentWinForms/DivSegmentWinForms/Form1.cs
@@ -21,31 +21,30 @@ namespace DivSegmentWinForms
   
         public static double Function(double x)
         {
-            return ((x * x * x * x) + 8 * (x * x * x) - 6 * (x * x) - 72 * x + 90);  
+            return ((x * x * x * x) + 8 * (x * x * x) - 6 * (x * x) - 72 * x + 90);  // заданная функция
         }
         public  void DS()
         {
 
-            double a = -6, b = 2.0, x_m = (a + b) / 2; 
+            double a = -6, b = 2.0, x_m = (a + b) / 2; // заданный по условию сегмент
             double y, z, f1, f2, f3, eps = 0.0001;
             double res_x;
-           
+            // данное поэтапное решение соответствует алгоритму из Пантелеева (стр. 112)  
             do
             {
-                
 
                 y = a + (b - a) / 4;
                 z = b - (b - a) / 4;
                 f1 = Function(y);
                 f2 = Function(z);
                 f3 = Function(x_m);
-                
+                // Согласно шагу 5 алгоритма, описанному в Пантелееве (стр. 11), выполняем действия при f(y_k) < f(x_m)
                 if (f1 < f3)
                 {
                     b = x_m;
                     x_m = y;
                 }
-           
+                // Согласно шагу 6 алгоритма, описанному в Пантелееве (стр. 112), выполняем действия при f(z_k) < f(x_m)
                 else if (f2 < f3)
                 {
                     a = x_m;
@@ -57,8 +56,8 @@ namespace DivSegmentWinForms
                     b = z;
                 }
             }
-            while (Math.Abs(b - a) > eps); 
-            res_x = (a + b) / 2; 
+            while (Math.Abs(b - a) > eps); // условие завершения программы
+            res_x = (a + b) / 2; // в качестве приближённого решения берём середину последнего интервала
 
             textBox1.Text = "Результат: x = " + res_x;
             textBox2.Text = "Значение функции: F(x) = " + Function(res_x);
