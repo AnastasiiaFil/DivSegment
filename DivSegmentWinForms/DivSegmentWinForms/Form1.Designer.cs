d1a869da9bc02bed68fe8773707ef05ca1fbcdf3
AnastasiiaFil <40761069+AnastasiiaFil@users.noreply.github.com>
2018-07-02 22:15:42 +0300

    The implementation of the method of division of a segment in half was added

--- DivSegmentWinForms/DivSegmentWinForms/Form1.Designer.cs
+++ DivSegmentWinForms/DivSegmentWinForms/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(536, 466);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(528, 437);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 466);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- DivSegmentWinForms/DivSegmentWinForms/Form1.cs
+++ DivSegmentWinForms/DivSegmentWinForms/Form1.cs
@@ -15,6 +15,54 @@ namespace DivSegmentWinForms
         public Form1()
         {
             InitializeComponent();
+            DS();
+        }
+
+  
+        public static double Function(double x)
+        {
+            return ((x * x * x * x) + 8 * (x * x * x) - 6 * (x * x) - 72 * x + 90);  
+        }
+        public  void DS()
+        {
+
+            double a = -6, b = 2.0, x_m = (a + b) / 2; 
+            double y, z, f1, f2, f3, eps = 0.0001;
+            double res_x;
+           
+            do
+            {
+                
+
+                y = a + (b - a) / 4;
+                z = b - (b - a) / 4;
+                f1 = Function(y);
+                f2 = Function(z);
+                f3 = Function(x_m);
+                
+                if (f1 < f3)
+                {
+                    b = x_m;
+                    x_m = y;
+                }
+           
+                else if (f2 < f3)
+                {
+                    a = x_m;
+                    x_m = z;
+                }
+                else if (f2 >= f3)
+                {
+                    a = y;
+                    b = z;
+                }
+            }
+            while (Math.Abs(b - a) > eps); 
+            res_x = (a + b) / 2; 
+
+            textBox1.Text = "Результат: x = " + res_x;
+            textBox2.Text = "Значение функции: F(x) = " + Function(res_x);
+
         }
     }
 }
